433dafa03e7d59630acfba10f20facd6ff7bf4b2
grega1 <adrianogrego1@gmail.com>
2023-08-31 10:59:23 +0100

    fix: updating ReadME.md and changing the default picture

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,8 +1,12 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext } from "react";
 import "./index.css";
 import Filter from "../../components/Filter";
+
+import catMouse from "../../assets/cat-mouse.gif"
 import { CatsService } from "../../services/";
 import { CatsContext } from "../../contexts/CatContext";
+
+
 export default function Home() {
   const { cat, setCat } = useContext(CatsContext);
 
@@ -18,8 +22,9 @@ export default function Home() {
       <div className="cat-viewer">
         {cat ? (
           <img src={cat} alt="cat" />
-        ) : (
-          <h1>Finding cats...</h1>
+        ) : (<p> Searching cats...
+          <img src={catMouse} alt="cat-searching" />
+          </p>
         )}
       </div>
     </div>
